e846b9b0e0a1b90caae051fb0205d7042474765c
Alina8177 <83888270+Alina8177@users.noreply.github.com>
2021-12-19 13:33:39 -0500

    added gym model & initial migrate

--- apps/gym/models.py
+++ apps/gym/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Gym(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    location = models.CharField(max_length=100, null=True, blank=False)
+    zip_code = models.CharField(max_length=50, null=True, blank=True)
