6299b1790fdf3f3426fe56664b779ebc971d4bc2
Alina8177 <83888270+Alina8177@users.noreply.github.com>
2021-12-19 14:07:20 -0500

    added program model

--- apps/gym/admin.py
+++ apps/gym/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from apps.gym.models import Gym
 # Register your models here.
+
+@admin.register(Gym)
+class GymAdmin(admin.ModelAdmin):
+    list_display = ["name", "location", "zip_code"]
+
+    search_fields = ["name", "location",]
--- apps/gym/models.py
+++ apps/gym/models.py
@@ -6,3 +6,11 @@ class Gym(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False, unique=True)
     location = models.CharField(max_length=100, null=True, blank=False)
     zip_code = models.CharField(max_length=50, null=True, blank=True)
+
+
+class Program(models.Model):
+    name = models.CharField(max_length=40, null=False, blank=False)
+    description = models.TextField()
+    gym = models.ForeignKey("gym.Gym", null=True, blank=True, on_delete=models.CASCADE, related_name='programs')
+    is_archive = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
