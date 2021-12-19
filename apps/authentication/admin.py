c047ccf11c446ce8e625c27b2984ce2961cf77c9
Alina8177 <83888270+Alina8177@users.noreply.github.com>
2021-12-19 13:04:15 -0500

    added CRUD for user in django-admin

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from apps.authentication.models import User
+from .forms import UserCreationForm, UserChangeForm
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ['email',]
+
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    add_fieldsets = ((None, {"fields": ("email", "password1", "password2")}),)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+    )
+
+    ordering = ['-id']
