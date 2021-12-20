5b1d33ff7b3665e08b3a02ef9d7095b908c9eb69
Alina8177 <83888270+Alina8177@users.noreply.github.com>
2021-12-20 06:34:58 -0500

    added payment

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -41,3 +41,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     
     def __str__(self) -> str:
         return self.email
+
+
+class PaymentStatus(models.TextChoices):
+    PAID = ("paid", "Paid")
+    CANCELED = ("canceled", "Canceled")
+
+class Payment(models.Model):
+    user = models.ForeignKey("authentication.User", on_delete=models.CASCADE, null=True, related_name='payments')
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, default=PaymentStatus.PAID, choices=PaymentStatus.choices)
